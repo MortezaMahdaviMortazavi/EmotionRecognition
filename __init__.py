c9d492e96cc4dde8b626a65a004da269c0a1beef
Morteza Mahdavi <98619035+MortezaMahdaviMortazavi@users.noreply.github.com>
2023-09-26 14:51:28 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,41 +1,54 @@
-        tp = (y_true_one_hot * y_pred).sum(dim=0).to(torch.float32)
-        tn = ((1 - y_true_one_hot) * (1 - y_pred)).sum(dim=0).to(torch.float32)
-        fp = ((1 - y_true_one_hot) * y_pred).sum(dim=0).to(torch.float32)
-        fn = (y_true_one_hot * (1 - y_pred)).sum(dim=0).to(torch.float32)
+def predict(texts, local_ids, model, device, threshold=0.5):
+    model.eval()  # Set the model to evaluation 
 
-        precision = tp / (tp + fp + self.epsilon)
-        recall = tp / (tp + fn + self.epsilon)
+    inputs = []  # To store the input tensors
+    label_dict = {0: 'sadness', 1: 'happiness', 2: 'surprise', 3: 'fear', 4: 'disgust', 5: 'anger', 6: 'other'}
+    num_classes = len(label_dict)
 
-        f1 = 2 * (precision * recall) / (precision + recall + self.epsilon)
-        f1 = f1.clamp(min=self.epsilon, max=1-self.epsilon)
-        f1 = f1.detach()
+    tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-large')
+    max_length = 64
+    for text in tqdm(texts):
+        encoding = tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            truncation=True,
+            max_length=max_length,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
+        input_ids = encoding['input_ids'].squeeze().to(device)
+        attention_mask = encoding['attention_mask'].squeeze().to(device)
+        inputs.append({'input_ids': input_ids, 'attention_mask': attention_mask})
 
-        CE = nn.CrossEntropyLoss()(y_pred, y_true)
-        loss = CE - f1.mean()
-        
-        return loss
+    with torch.no_grad():
+        predictions = []
+        prob_matrix = []  # To store the probability distribution for each text
+        for input_data in tqdm(inputs):
+            input_ids = input_data['input_ids'].unsqueeze(0).to(device)  # Add batch dimension
+            attention_mask = input_data['attention_mask'].unsqueeze(0).to(device)  # Add batch dimension
 
+            outputs = model(input_ids, attention_mask)
+            predicted_probs = torch.softmax(outputs, dim=1).squeeze().cpu().numpy()
+            prob_matrix.append(predicted_probs)
 
-class XLMCnnLstm(nn.Module):
-    def __init__(self, num_classes):
-        super(XLMCnnLstm, self).__init__()
-        self.bert = AutoModel.from_pretrained("xlm-roberta-base")
-        self.conv1d = nn.Conv1d(in_channels=768, out_channels=256, kernel_size=3)
-        self.maxpool = nn.MaxPool1d(kernel_size=2)
-        self.lstm = nn.LSTM(input_size=256, hidden_size=128, num_layers=1, bidirectional=False)
-        self.dropout = nn.Dropout(p=0.4)
-        self.dense = nn.Linear(128, num_classes)
-        
-    def forward(self, input_ids, attention_mask):
-        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        pooled_output = outputs.last_hidden_state
-        pooled_output = pooled_output.permute(0, 2, 1)  # Reshaping for Conv1d
-        conv_output = self.conv1d(pooled_output)
-        conv_output = torch.relu(conv_output)
-        pooled_output = self.maxpool(conv_output)
-        pooled_output = pooled_output.permute(0, 2, 1)  # Reshaping for LSTM
-        lstm_output, _ = self.lstm(pooled_output)
-        lstm_output = lstm_output[:, -1, :]  # Taking the last hidden state
-        dropout_output = self.dropout(lstm_output)
-        logits = self.dense(dropout_output)
-        return logits
+            # Determine primary emotion based on probability threshold
+            max_prob = torch.max(outputs, dim=1)[0].item()
+            if max_prob > threshold:
+                predicted_label = label_dict[torch.argmax(outputs, dim=1).item()]
+            else:
+                predicted_label = 'other'
+            predictions.append(predicted_label)
+
+    csv_data = []
+    for local_id, text, primary_emotion, probs in zip(local_ids, texts, predictions, prob_matrix):
+        emotion_probs = [1 if prob > threshold else 0 for prob in probs]
+        row = [local_id, text, primary_emotion] + emotion_probs
+        csv_data.append(row)
+
+    # Create a DataFrame from the CSV data
+    columns = ["local_id", "tweet", "primary_emotion", "anger", "sadness", "fear", "happiness", "disgust", "surprise", "other"]
+    df = pd.DataFrame(csv_data, columns=columns)
+    df.to_csv('predictions_large_gru.csv', index=False)
+
+    return predictions
